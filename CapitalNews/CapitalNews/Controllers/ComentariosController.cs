d7ea5beeac9e1107cb332ca33067bfb01846b7c9
Pedro Gomes <aluno21279@ipt.pt>
2022-05-28 16:47:51 +0100

    Adição dos controllers JornalistasNoticias e Comentarios

---
+++ CapitalNews/CapitalNews/Controllers/ComentariosController.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using CapitalNews.Data;
+using CapitalNews.Models;
+
+namespace CapitalNews.Controllers
+{
+    public class ComentariosController : Controller
+    {
+        private readonly CapitalDb _context;
+
+        public ComentariosController(CapitalDb context)
+        {
+            _context = context;
+        }
+
+        // GET: Comentarios
+        public async Task<IActionResult> Index()
+        {
+            var capitalDb = _context.Comentarios.Include(c => c.Leitor).Include(c => c.Noticia);
+            return View(await capitalDb.ToListAsync());
+        }
+
+        // GET: Comentarios/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Comentarios == null)
+            {
+                return NotFound();
+            }
+
+            var comentarios = await _context.Comentarios
+                .Include(c => c.Leitor)
+                .Include(c => c.Noticia)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (comentarios == null)
+            {
+                return NotFound();
+            }
+
+            return View(comentarios);
+        }
+
+        // GET: Comentarios/Create
+        public IActionResult Create()
+        {
+            ViewData["LeitorFK"] = new SelectList(_context.Leitores, "Id", "Nome");
+            ViewData["NoticiaFK"] = new SelectList(_context.Noticias, "Id", "Id");
+            return View();
+        }
+
+        // POST: Comentarios/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,TextoComentario,DataComentario,Visibilidade,NoticiaFK,LeitorFK")] Comentarios comentarios)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(comentarios);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["LeitorFK"] = new SelectList(_context.Leitores, "Id", "Nome", comentarios.LeitorFK);
+            ViewData["NoticiaFK"] = new SelectList(_context.Noticias, "Id", "Id", comentarios.NoticiaFK);
+            return View(comentarios);
+        }
+
+        // GET: Comentarios/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Comentarios == null)
+            {
+                return NotFound();
+            }
+
+            var comentarios = await _context.Comentarios.FindAsync(id);
+            if (comentarios == null)
+            {
+                return NotFound();
+            }
+            ViewData["LeitorFK"] = new SelectList(_context.Leitores, "Id", "Nome", comentarios.LeitorFK);
+            ViewData["NoticiaFK"] = new SelectList(_context.Noticias, "Id", "Id", comentarios.NoticiaFK);
+            return View(comentarios);
+        }
+
+        // POST: Comentarios/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,TextoComentario,DataComentario,Visibilidade,NoticiaFK,LeitorFK")] Comentarios comentarios)
+        {
+            if (id != comentarios.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(comentarios);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ComentariosExists(comentarios.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["LeitorFK"] = new SelectList(_context.Leitores, "Id", "Nome", comentarios.LeitorFK);
+            ViewData["NoticiaFK"] = new SelectList(_context.Noticias, "Id", "Id", comentarios.NoticiaFK);
+            return View(comentarios);
+        }
+
+        // GET: Comentarios/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Comentarios == null)
+            {
+                return NotFound();
+            }
+
+            var comentarios = await _context.Comentarios
+                .Include(c => c.Leitor)
+                .Include(c => c.Noticia)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (comentarios == null)
+            {
+                return NotFound();
+            }
+
+            return View(comentarios);
+        }
+
+        // POST: Comentarios/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Comentarios == null)
+            {
+                return Problem("Entity set 'CapitalDb.Comentarios'  is null.");
+            }
+            var comentarios = await _context.Comentarios.FindAsync(id);
+            if (comentarios != null)
+            {
+                _context.Comentarios.Remove(comentarios);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ComentariosExists(int id)
+        {
+          return _context.Comentarios.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ CapitalNews/CapitalNews/Controllers/JornalistasNoticiasController.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using CapitalNews.Data;
+using CapitalNews.Models;
+
+namespace CapitalNews.Controllers
+{
+    public class JornalistasNoticiasController : Controller
+    {
+        private readonly CapitalDb _context;
+
+        public JornalistasNoticiasController(CapitalDb context)
+        {
+            _context = context;
+        }
+
+        // GET: JornalistasNoticias
+        public async Task<IActionResult> Index()
+        {
+            var capitalDb = _context.JornalistasNoticias.Include(j => j.Jornalista).Include(j => j.Noticia);
+            return View(await capitalDb.ToListAsync());
+        }
+
+        // GET: JornalistasNoticias/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.JornalistasNoticias == null)
+            {
+                return NotFound();
+            }
+
+            var jornalistasNoticias = await _context.JornalistasNoticias
+                .Include(j => j.Jornalista)
+                .Include(j => j.Noticia)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (jornalistasNoticias == null)
+            {
+                return NotFound();
+            }
+
+            return View(jornalistasNoticias);
+        }
+
+        // GET: JornalistasNoticias/Create
+        public IActionResult Create()
+        {
+            ViewData["JornalistaFK"] = new SelectList(_context.Jornalistas, "Id", "Id");
+            ViewData["NoticiaFK"] = new SelectList(_context.Noticias, "Id", "Id");
+            return View();
+        }
+
+        // POST: JornalistasNoticias/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,NoticiaFK,JornalistaFK")] JornalistasNoticias jornalistasNoticias)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(jornalistasNoticias);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["JornalistaFK"] = new SelectList(_context.Jornalistas, "Id", "Id", jornalistasNoticias.JornalistaFK);
+            ViewData["NoticiaFK"] = new SelectList(_context.Noticias, "Id", "Id", jornalistasNoticias.NoticiaFK);
+            return View(jornalistasNoticias);
+        }
+
+        // GET: JornalistasNoticias/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.JornalistasNoticias == null)
+            {
+                return NotFound();
+            }
+
+            var jornalistasNoticias = await _context.JornalistasNoticias.FindAsync(id);
+            if (jornalistasNoticias == null)
+            {
+                return NotFound();
+            }
+            ViewData["JornalistaFK"] = new SelectList(_context.Jornalistas, "Id", "Id", jornalistasNoticias.JornalistaFK);
+            ViewData["NoticiaFK"] = new SelectList(_context.Noticias, "Id", "Id", jornalistasNoticias.NoticiaFK);
+            return View(jornalistasNoticias);
+        }
+
+        // POST: JornalistasNoticias/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,NoticiaFK,JornalistaFK")] JornalistasNoticias jornalistasNoticias)
+        {
+            if (id != jornalistasNoticias.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(jornalistasNoticias);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!JornalistasNoticiasExists(jornalistasNoticias.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["JornalistaFK"] = new SelectList(_context.Jornalistas, "Id", "Id", jornalistasNoticias.JornalistaFK);
+            ViewData["NoticiaFK"] = new SelectList(_context.Noticias, "Id", "Id", jornalistasNoticias.NoticiaFK);
+            return View(jornalistasNoticias);
+        }
+
+        // GET: JornalistasNoticias/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.JornalistasNoticias == null)
+            {
+                return NotFound();
+            }
+
+            var jornalistasNoticias = await _context.JornalistasNoticias
+                .Include(j => j.Jornalista)
+                .Include(j => j.Noticia)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (jornalistasNoticias == null)
+            {
+                return NotFound();
+            }
+
+            return View(jornalistasNoticias);
+        }
+
+        // POST: JornalistasNoticias/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.JornalistasNoticias == null)
+            {
+                return Problem("Entity set 'CapitalDb.JornalistasNoticias'  is null.");
+            }
+            var jornalistasNoticias = await _context.JornalistasNoticias.FindAsync(id);
+            if (jornalistasNoticias != null)
+            {
+                _context.JornalistasNoticias.Remove(jornalistasNoticias);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool JornalistasNoticiasExists(int id)
+        {
+          return _context.JornalistasNoticias.Any(e => e.Id == id);
+        }
+    }
+}
