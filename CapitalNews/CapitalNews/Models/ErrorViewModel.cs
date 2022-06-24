082fc4185849011e974793c4908ac6b2b8506c70
Pedro Gomes <aluno21279@ipt.pt>
2022-06-24 15:45:13 +0100

    Alterações visuais

--- CapitalNews/CapitalNews/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace CapitalNews.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
---
+++ CapitalNews/CapitalNews/Models/ViewModel.cs
@@ -0,0 +1,40 @@
+namespace CapitalNews.Models
+{
+    public class CategoriasViewModel
+    {
+        public int Id { get; set; }
+        public string CategoriaNome { get; set; }
+    }
+
+    public class FotografiasViewModel
+    {
+        public int Id { get; set; }
+        public string Descritor { get; set; }
+        public string Imagem { get; set; }
+    }
+
+    public class JornalistasViewModel
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string FotoJor { get; set; }
+    }
+
+    public class NoticiasViewModel
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public string Body { get; set; }
+        public string Categoria { get; set; }
+        public DateTime Data { get; set; }
+        public string Fotografia { get; set; }
+        public string Jornalista { get; set; }
+    }
+
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
