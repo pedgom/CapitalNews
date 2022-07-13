7a9c20128d2172a67b9d19b8c47fd75d5618136a
João Silva <js84523@gmail.com>
2022-07-14 00:49:48 +0100

    estagnação do codigo
    atualizar tudo para poder reverter alguns commits

---
+++ CapitalNews/CapitalNews/Data/Migrations/20220713183004_reparar.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CapitalNews.Data.Migrations
+{
+    public partial class reparar : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "a",
+                column: "ConcurrencyStamp",
+                value: "220b419b-11cb-4a98-9aca-755dd80018fd");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "j",
+                column: "ConcurrencyStamp",
+                value: "3dec2edc-72d1-4764-ad82-1f9f225da657");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "l",
+                column: "ConcurrencyStamp",
+                value: "9d4772d3-5b06-4c90-9d7e-58e519f8abdf");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "a",
+                column: "ConcurrencyStamp",
+                value: "0028d95f-fab8-4e8d-978f-4eab02d91a99");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "j",
+                column: "ConcurrencyStamp",
+                value: "db02bc9a-f1b2-4324-b6cc-4504567d76c5");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "l",
+                column: "ConcurrencyStamp",
+                value: "be2c6ab1-dbdf-4bd1-82ab-5d1be0edc83e");
+        }
+    }
+}
